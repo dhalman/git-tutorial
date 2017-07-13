bd112310ce209240162d36ee9822e5c777740577
Jordan Castillo <jordancastillo@pcp129283pcs.wireless.calpoly.edu>
2017-07-13 09:56:33 -0700

    This is my first commit! I wonder how I will be able to read this later, or if this will even be read. Very cool lab.

---
+++ SidebarMenu/customViewController.swift
@@ -0,0 +1,35 @@
+//
+//  customViewController.swift
+//  SidebarMenu
+//
+//  Created by Jordan Castillo on 7/13/17.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class customViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
