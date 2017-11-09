86ef220743910565663e81a86150516314f2da00
aly-chapman <aly.chapman3@gmail.com>
2017-11-09 10:29:43 -0800

    Added new page
    Added new page segued off of news page

--- SidebarMenu/NewsTableViewController.swift
+++ SidebarMenu/NewsTableViewController.swift
@@ -75,7 +75,10 @@ class NewsTableViewController: UITableViewController {
         return cell
     }
     
-
+   /* override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "pushSegue", sender: nil)
+    }
+*/
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
