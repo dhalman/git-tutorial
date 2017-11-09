4897b18a9991d7e8f172c74f663e2f46fb1cb6bf
jeffclam <jeffrey.clam95@gmail.com>
2017-11-09 10:30:13 -0800

    Delete NewScreenViewController.swift

--- SidebarMenu/NewScreenViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NewScreenViewController.swift
-//  SidebarMenu
-//
-//  Created by Local Account 436-18 on 11/9/17.
-//  Copyright © 2017 AppCoda. All rights reserved.
-//
-
-import Foundation
