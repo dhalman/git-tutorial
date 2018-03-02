17c80ab74cb5327e0c5d14acc2246d70b3b59618
Chandler Warne <chandler@appdrawerllc.com>
2018-03-02 11:32:19 -0800

    Added new segue screen for news article selection.

---
+++ SidebarMenu/MyNewViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MyNewViewController.swift
+//
+//  Created by Chandler Warne.
+//  Copyright (c) 2017 Chandler Warne. All rights reserved.
+//
+
+import UIKit
+
+class MyNewViewController: UIViewController {
+    @IBOutlet weak var menuButton:UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
