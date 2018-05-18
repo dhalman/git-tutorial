6c9191b490ba643f57f4854581291c239035a6e4
zmonroe <zjmonroe23@gmail.com>
2018-05-18 13:33:47 -0700

    Added alert before segue

--- SidebarMenu/NewsTableViewController.swift
+++ SidebarMenu/NewsTableViewController.swift
@@ -34,7 +34,12 @@ class NewsTableViewController: UITableViewController
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        performSegue(withIdentifier: "myPush", sender: self)
+        let alert = UIAlertController(title: "Alert", message: "Move to new view", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action -> Void in
+            self.performSegue(withIdentifier: "myPush", sender: nil)
+        }))
+        alert.addAction(UIAlertAction(title: "No", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
